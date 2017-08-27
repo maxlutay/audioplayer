1a4e27f2cd291b15893e1aa68e1dcceff7740472
MaximLutay <maksym.lutai@nure.ua>
2017-08-27 18:01:04 +0300

    npm && webpack config + dependencies

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require("webpack");
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
+
+
+module.exports = {
+    entry:  {
+        index: "./src/index.js"
+    }
+    ,output: {
+        filename: "bundle.js"
+        ,path: path.resolve("./dist")
+        ,pathinfo:true
+    }
+    ,module: {
+        rules: [
+            {
+                test: /\.css$/
+                ,use: ["style-loader","css-loader"] 
+                ,include: [
+                    path.resolve("src")
+                ]
+            }
+            ,{
+                test: /\.html$/
+                ,loader: "file-loader"
+                ,options: {
+                    name: "[name].[ext]"
+                }
+                ,include: [
+                    path.resolve("src")
+                ]
+            }
+        ]
+    }
+    ,plugins: [
+        new CleanPlugin(["./dist"])
+    ]
+
+}
