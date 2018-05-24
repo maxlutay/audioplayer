75c888152634273b9fd889f8d5ce20520fd42e9e
Maxim Lutay <maksym.lutai@nure.ua>
2018-05-24 14:50:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ require("./index.html");
 require("./styles.css");
 require("./app/app.js");
 
-require("./media/data-0.mp3");
-require("./media/data-0 (1).mp3");
-require("./media/data-0 (2).mp3");
-require("./media/data-0 (3).mp3");
-require("./media/data-0 (4).mp3");
+//require("./media/data-0.mp3");
+
 
 const Vue = require("vue/dist/vue.js");
 
